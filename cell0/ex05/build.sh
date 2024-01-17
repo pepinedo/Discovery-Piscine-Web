929dc7a780589ce114a42f85f6cba065cbd16974
ppinedo- <ppinedo-@student.42malaga.com>
2024-01-17 20:02:20 +0100

    build.sh

--- cell0/ex05/build.sh
+++ cell0/ex05/build.sh
@@ -1,11 +1,11 @@
-i=1
 if [ -z $1 ]
 then
 	echo "No arguments supplied"
 	exit
 fi
-while [ i<=$# ]
-do
-	mkdir ex$i
-	i++
+
+for nombre in "$@"; do
+	mkdir "ex$nombre"
 done
+
+echo "Carpetas creadas con éxito: $@"
